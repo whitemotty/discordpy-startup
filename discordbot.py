a2b0fc2aff6b8fb3328ba10fb724b3fda32356a2
whitemotty <ys199877@yahoo.co.jp>
2020-11-10 16:57:24 +0900

    change 8

--- discordbot.py
+++ discordbot.py
@@ -20,7 +20,7 @@ async def on_message(message):
     if random.randrange(10) < 2:
         if message.author.bot:
             return
-        if message.content.startswith(':poop:'):
+        if message.content.startswith('💩'):
             await message.channel.send(':poop:')
         if message.content.startswith('<@!775343042567340053>'):
             await message.channel.send('草')
