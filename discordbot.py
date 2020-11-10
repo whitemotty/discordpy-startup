53b07e1e7eed4ec81e2543744995779645d7ff47
whitemotty <ys199877@yahoo.co.jp>
2020-11-10 12:51:20 +0900

    add 3

--- discordbot.py
+++ discordbot.py
@@ -1,9 +1,10 @@
 from discord.ext import commands
+import discord
 import os
 import traceback
 import random
 
-bot = commands.Bot(command_prefix='*',activity=discord.Game("マックのバイト"))
+bot = commands.Bot(command_prefix='*', activity=discord.Game("マックのバイト"))
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
@@ -12,7 +13,7 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
-    
+
 
 @bot.event
 async def on_message(message):
