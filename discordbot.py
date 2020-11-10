78322f6124bf2e7efa10955307be4a3b8cdc8505
whitemotty <ys199877@yahoo.co.jp>
2020-11-10 12:43:05 +0900

    add 1

--- discordbot.py
+++ discordbot.py
@@ -1,8 +1,9 @@
 from discord.ext import commands
 import os
 import traceback
+import random
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='*',activity=discord.Game("マックのバイト"))
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
@@ -11,11 +12,36 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
+    
 
+@bot.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content in '<@!337590899775242240>':
+        await message.channel.send('にゃーん')
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def job(ctx):
+    await ctx.send('今日はオープンクローズかな')
 
+@bot.command()
+async def today(ctx):
+    await ctx.send('すまねぇ、今日は遊べないや')
+
+@bot.command()
+async def help(ctx):
+    embed = discord.Embed(title="ゆーしbot help", description="A Very Nice ゆーしbot. List of commands are:", color=0xeee657)
+
+    embed.add_field(name="mention to ゆーしbot", value="笑われます", inline=False)
+    embed.add_field(name="mention to オープンクローズ", value="ゆーしの代わりに返信します", inline=False)
+    
+    embed.add_field(name="*help", value="コマンド一覧の表示をします", inline=False)
+    embed.add_field(name="*job", value="今日のバイトのシフトを伝えます", inline=False)
+    embed.add_field(name="*today", value="今日の予定を伝えます", inline=False)
+
+    await ctx.send(embed=embed)
 
 bot.run(token)
