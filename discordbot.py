37cecbee1994456f98c682f3570b1f5b7a5b9312
whitemotty <ys199877@yahoo.co.jp>
2020-11-10 18:44:45 +0900

    change channel

--- discordbot.py
+++ discordbot.py
@@ -58,9 +58,9 @@ async def on_voice_state_update(member, before, after):
     if member.guild.id == 644381235753385985 and (before.channel != after.channel):
         alert_channel = bot.get_channel(644381236424343552)
         if before.channel is None: 
-            embed = discord.Embed(title:"通話開始", description:'通話が開始されました．俺はバイトあるから行けないや.', color:67CCAAFF,
-                                  fields:[{name:"`ボイスチャンネル`", value:after.channel.name, inline:true},
-                                          {name:"`開始者`", value:menber.name, inline:true}])
+            embed = discord.Embed(title="通話開始", description='通話が開始されました．俺はバイトあるから行けないや.', color=67CCAAFF)
+            embed.add_field(name="`ボイスチャンネル`", value=after.channel.name, inline=true)
+            embed.add_field(name="`開始者`", value=menber.name, inline=true)
             await alert_channel.send(embed)
 
 bot.run(token)
